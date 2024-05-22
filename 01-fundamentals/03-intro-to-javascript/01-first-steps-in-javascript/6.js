e88fbfa2ccc69c46747db87c58f7267e59592eb1
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-05-22 10:33:52 -0300

    added first steps in javascript exercises (#13)

---
+++ 01-fundamentals/03-intro-to-javascript/01-first-steps-in-javascript/6.js
@@ -0,0 +1,7 @@
+// there's a pair between us (only 1 if should be used)
+let num1 = 4,
+  num2 = 5,
+  num3 = 6;
+
+if (!(num1 % 2) || !(num2 % 2) || !(num3 % 2)) console.log(true);
+else console.log(false);
