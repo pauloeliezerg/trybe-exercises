597e33e964e16c360133f02f2b2f2921c745f266
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-06 02:13:51 -0300

    added exercise 2.2

--- 01-fundamentals/05-html-and-css-forms-flexbox-and-reponsive/01-forms/exercise-02/script.js
+++ 01-fundamentals/05-html-and-css-forms-flexbox-and-reponsive/01-forms/exercise-02/script.js
@@ -5,3 +5,6 @@ const hrefLink = document.querySelector("#href");
 
 // 1. add a click event to the element that has the id href. In the event function, implement the preventDefault method to prevent the element's default behavior. In other words, when you click on the link, nothing should happen
 hrefLink.addEventListener("click", (event) => event.preventDefault());
+
+// 2. add a click event on the checkbox element that has the input-checkbox id. In the event function, implement the preventDefault method to prevent the element's default behavior. In other words, when you click on the box, nothing should happen
+inputCheckbox.addEventListener("click", (event) => event.preventDefault());
