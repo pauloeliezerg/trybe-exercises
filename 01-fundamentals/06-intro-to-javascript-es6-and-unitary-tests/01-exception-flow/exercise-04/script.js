bb0bbcc2cb228a3fe468e4d5f454de8dadd353db
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-10 18:56:47 -0300

    added exercise 4.1

--- 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/01-exception-flow/exercise-04/script.js
+++ 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/01-exception-flow/exercise-04/script.js
@@ -1,3 +1,4 @@
+// 1. if the function does not receive name or age (or both), throw an exception with the message 'All information is required'
 const studentRegister = (name, age) => {
-  // seu código aqui
+  if (!name || !age) throw new Error("All information is required");
 };
