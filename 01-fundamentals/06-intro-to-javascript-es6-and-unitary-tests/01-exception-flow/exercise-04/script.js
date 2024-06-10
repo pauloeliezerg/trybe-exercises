60a4bd2f3f2fb4fd52153a82c316710980aeb2d7
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-10 18:58:47 -0300

    added exercise 4.2

--- 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/01-exception-flow/exercise-04/script.js
+++ 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/01-exception-flow/exercise-04/script.js
@@ -1,4 +1,7 @@
 // 1. if the function does not receive name or age (or both), throw an exception with the message 'All information is required'
+// 2. if the role receives the name and an age below 18 years old, throw an exception with the message 'Oops, unfortunately you can't take classes at the moment'
 const studentRegister = (name, age) => {
   if (!name || !age) throw new Error("All information is required");
+  if (age < 18)
+    throw new Error("Oops, unfortunately you can't take classes at the moment");
 };
