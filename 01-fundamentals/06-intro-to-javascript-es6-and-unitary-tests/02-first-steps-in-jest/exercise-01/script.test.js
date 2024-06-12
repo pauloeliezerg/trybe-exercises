7d6845026ebb9665eb83604ae82ab6273f6b9468
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-12 04:58:03 -0300

    added exercise 1.2

--- 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/02-first-steps-in-jest/exercise-01/script.test.js
+++ 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/02-first-steps-in-jest/exercise-01/script.test.js
@@ -4,4 +4,7 @@ describe("test removeItem function", () => {
   it("returns the expected array", () => {
     expect(removeItem([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
+  it("does not return unexpected array", () => {
+    expect(removeItem([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
 });
