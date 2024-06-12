bb189ba3eb8eae7de99dad4e805fff4aae01496f
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-12 07:22:49 -0300

    refactored test structure with 2 describes and added exercise 3.2

--- 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/02-first-steps-in-jest/exercise-03/script.test.js
+++ 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/02-first-steps-in-jest/exercise-03/script.test.js
@@ -1,9 +1,27 @@
 const { encode, decode } = require("./script");
 
-describe("tests the encode and decode functions", () => {
+describe("tests the encode function", () => {
   it("if encode is a function", () => {
     expect(typeof encode).toBe("function");
   });
+  it(`if "a" returns "1"`, () => {
+    expect(encode("a")).toBe("1");
+  });
+  it(`if "e" returns "2"`, () => {
+    expect(encode("e")).toBe("2");
+  });
+  it(`if "i" returns "3"`, () => {
+    expect(encode("i")).toBe("3");
+  });
+  it(`if "o" returns "4"`, () => {
+    expect(encode("o")).toBe("4");
+  });
+  it(`if "u" returns "5"`, () => {
+    expect(encode("u")).toBe("5");
+  });
+});
+
+describe("tests the decode function", () => {
   it("if decode is a function", () => {
     expect(typeof decode).toBe("function");
   });
