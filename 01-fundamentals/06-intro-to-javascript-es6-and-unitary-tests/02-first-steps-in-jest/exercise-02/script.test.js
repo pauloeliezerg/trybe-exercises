1529c54182c6555632bcccdc11cdb071595e9427
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-12 05:47:36 -0300

    added exercise 2.4

--- 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/02-first-steps-in-jest/exercise-02/script.test.js
+++ 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/02-first-steps-in-jest/exercise-02/script.test.js
@@ -1,13 +1,16 @@
 const myFizzBuzz = require("./script");
 
 describe("test myFizzBuzz function", () => {
-  it(`If num is a number divisible by 3 and 5, the function returns "fizzbuzz"`, () => {
+  it(`if num is a number divisible by 3 and 5, the function returns "fizzbuzz"`, () => {
     expect(myFizzBuzz(15)).toBe("fizzbuzz");
   });
-  it(`If num is a number divisible only by 3(and not 5), the function returns "fizz"`, () => {
+  it(`if num is a number divisible only by 3(and not 5), the function returns "fizz"`, () => {
     expect(myFizzBuzz(3)).toBe("fizz");
   });
-  it(`If num is a number divisible only by 5(and not 3), the function returns "buzz"`, () => {
+  it(`if num is a number divisible only by 5(and not 3), the function returns "buzz"`, () => {
     expect(myFizzBuzz(5)).toBe("buzz");
+  });
+  it(`if num is a number that is not divisible by either 3 or 5, the function returns the number itself num`, () => {
+    expect(myFizzBuzz(1)).toBe(1);
   });
 });
