4ca2610d3362918d6287959634db2f6efc94aed3
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-12 05:37:55 -0300

    added exercise 2.2

--- 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/02-first-steps-in-jest/exercise-02/script.test.js
+++ 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/02-first-steps-in-jest/exercise-02/script.test.js
@@ -4,4 +4,7 @@ describe("test myFizzBuzz function", () => {
   it(`If num is a number divisible by 3 and 5, the function returns "fizzbuzz"`, () => {
     expect(myFizzBuzz(15)).toBe("fizzbuzz");
   });
+  it(`If num is a number divisible only by 3(and not 5), the function returns "fizz"`, () => {
+    expect(myFizzBuzz(3)).toBe("fizz");
+  });
 });
