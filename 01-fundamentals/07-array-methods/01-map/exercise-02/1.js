14d5ab2bff8a3cc60a95e5c2ca0e1bc69ec50896
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-14 16:03:00 -0300

    added exercise 2.2

--- 01-fundamentals/07-array-methods/01-map/exercise-02/1.js
+++ 01-fundamentals/07-array-methods/01-map/exercise-02/1.js
@@ -4,5 +4,3 @@ const temperaturesCelsius = [23, 10, 32, 21, 47];
 const temperaturesFahrenheit = temperaturesCelsius.map(
   (temperature) => (temperature * 9) / 5 + 32,
 );
-
-console.log(temperaturesFahrenheit);
