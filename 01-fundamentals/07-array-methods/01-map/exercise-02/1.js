2a15a42fe801d4560d5bd62375ef7ed1de07a34f
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-14 15:56:28 -0300

    added exercise 2.1

---
+++ 01-fundamentals/07-array-methods/01-map/exercise-02/1.js
@@ -0,0 +1,8 @@
+// 1. given an array of temperatures in degrees Celsius, create a function that returns a new array with the temperatures converted to Fahrenheit. The conversion formula is: Fahrenheit = (Celsius * 9/5) + 32
+const temperaturesCelsius = [23, 10, 32, 21, 47];
+
+const temperaturesFahrenheit = temperaturesCelsius.map(
+  (temperature) => (temperature * 9) / 5 + 32,
+);
+
+console.log(temperaturesFahrenheit);
