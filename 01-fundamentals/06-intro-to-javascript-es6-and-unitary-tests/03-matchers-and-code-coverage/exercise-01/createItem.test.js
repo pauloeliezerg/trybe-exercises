7cd22023ef5b426c0a05bd76276d3cec4e535e76
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-14 01:05:49 -0300

    added exercise 3.1

--- 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/03-matchers-and-code-coverage/exercise-01/createItem.test.js
+++ 01-fundamentals/06-intro-to-javascript-es6-and-unitary-tests/03-matchers-and-code-coverage/exercise-01/createItem.test.js
@@ -1,7 +1,11 @@
 const createItem = require("./createItem");
 
 describe("a função createItem", () => {
-  it.todo("cria um item válido");
+  it("cria um item válido", () => {
+    const item = createItem("name", "kg", 1, 1);
+    const expectedResult = { name: "name", unit: "kg", price: 1, quantity: 1 };
+    expect(item).toEqual(expectedResult);
+  });
   it.todo("utiliza zero como quantidade padrão");
   it.todo("Lança um erro quando não recebe parâmetros");
   it.todo("Lança um erro se o nome do item não é uma string");
