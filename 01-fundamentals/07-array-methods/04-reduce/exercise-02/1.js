18826b2020b3bcf062bca44adefcac7ba2cbfd50
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-06-19 09:50:41 -0300

    added exercise-02

---
+++ 01-fundamentals/07-array-methods/04-reduce/exercise-02/1.js
@@ -0,0 +1,2 @@
+// 1. write a function called multiply that multiplies two numbers passed as arguments. Assign the default value 1, if no value is passed as a second parameter
+multiply = (a, b = 1) => a * b;
