f08af7bc80385be02ba45da4e3e4cad6bb309ff9
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-07-02 17:56:12 -0300

    added clipboard-copy package and functionality

--- 02-front-end/01-frontend-and-async-javascript-intro/01-development-environment/01-password-generator/src/main.js
+++ 02-front-end/01-frontend-and-async-javascript-intro/01-development-environment/01-password-generator/src/main.js
@@ -1,9 +1,16 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './style.css';
 
 const passBtn = document.querySelector('button');
 const displayPass = document.querySelector('h2');
 
 passBtn.addEventListener('click', () => {
-  displayPass.innerHTML = nanoid();
+  const generatedPass = nanoid();
+  displayPass.innerHTML = generatedPass;
+});
+
+displayPass.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Passsword copied!');
 });
