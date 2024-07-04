7b0a7c4fec50abbb5f120ba69d230a0d7a2f44fe
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-07-03 22:18:31 -0300

    retrieved user posts and showed them with fillPosts() function

--- 02-front-end/01-frontend-and-async-javascript-intro/02-async-javascript/01-microblogs/index.js
+++ 02-front-end/01-frontend-and-async-javascript-intro/02-async-javascript/01-microblogs/index.js
@@ -12,7 +12,7 @@ const usersSelect = document.querySelector('#users-select');
 
 const USERS_API = 'https://dummyjson.com/users';
 
-// fetch the https://dummyjson.com/users API to retrieve user information; after receiving the data, pass the array returned by the API with the user information to the fillUsersSelect function so that the page select is updated
+// 1. fetch the https://dummyjson.com/users API to retrieve user information; after receiving the data, pass the array returned by the API with the user information to the fillUsersSelect function so that the page select is updated
 fetch(USERS_API)
   .then((response) => response.json())
   .then((data) => fillUsersSelect(data.users));
@@ -20,5 +20,10 @@ fetch(USERS_API)
 usersSelect.addEventListener('change', () => {
   clearPageData();
   const UserID = usersSelect.value;
+  const POST_API = `https://dummyjson.com/posts/user/${UserID}`
 
+  // 2. in the initial structure of the project, there is already an eventListener for the select change event that triggers the clearPage function; after calling this function, fetch the API https://dummyjson.com/posts/user/{userID}; remember that you must get the id of the selected person through the value attribute of the select
+  fetch(POST_API)
+    .then((response) => response.json())
+    .then((data) => fillPosts(data.posts));
 });
