81c66ca0b8b6a4cc6b3189cf0586f8212fd8b491
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-07-11 01:16:21 -0300

    added/updated README files

---
+++ 02-front-end/02-react-intro/01-react-and-typescript-intro/01-lottery/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Greeting from './Greeting';
+import LuckyNumbers from './LuckyNumbers';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Greeting />
+      <LuckyNumbers />
+    </>
+  );
+}
+
+export default App;
