cefbaa4bdc788e2d08197ca135ef63901f0f40cc
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-07-11 03:07:16 -0300

    added state and functions for the app

--- 02-front-end/02-react-intro/04-states-and-events/01-developer-tools/src/App.tsx
+++ 02-front-end/02-react-intro/04-states-and-events/01-developer-tools/src/App.tsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+
 function App() {
+    const [index, setIndex] = useState(0);
+
+    const toolKit = [
+        'JavaScript',
+        'TypeScript',
+        'React',
+        'HTML',
+        'CSS',
+        'Node',
+        'Automated tests',
+    ];
+
+    function handleNextClick() {
+        if (index + 1 < toolKit.length) setIndex(index + 1);
+        else setIndex(0);
+    }
+
+    function handlePreviousClick() {
+        if (index - 1 >= 0) setIndex(index - 1);
+        else setIndex(toolKit.length - 1);
+    }
+
     return (
         <>
             <h1>Developer's Tools Box</h1>
-            <button>Previous</button>
-            <button>Next</button>
+            <h2>{ toolKit[index] }</h2>
+            <button onClick={ handlePreviousClick }>Previous</button>
+            <button onClick={ handleNextClick }>Next</button>
         </>
     );
 }
