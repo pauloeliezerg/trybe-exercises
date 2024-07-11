6d9ab1eab8dc998913d8bde1cfc270812d60306f
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-07-11 03:00:54 -0300

    created Developer Tools app

---
+++ 02-front-end/02-react-intro/04-states-and-events/01-developer-tools/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <h1>Developer's Tools Box</h1>
+            <button>Previous</button>
+            <button>Next</button>
+        </>
+    );
+}
+
+export default App;
