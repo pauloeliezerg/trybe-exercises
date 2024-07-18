8a398cad02118f04618f703003ca154483e3b9be
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-07-18 19:03:35 -0300

    added React Router elements

--- 02-front-end/03-deeper-dive-into-react/01-react-router/01-coffee-shop/src/App.tsx
+++ 02-front-end/03-deeper-dive-into-react/01-react-router/01-coffee-shop/src/App.tsx
@@ -1,7 +1,15 @@
+import Home from './pages/home';
+import CoffeeList from './pages/coffee-list';
+import Coffee from './pages/coffee';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
     return (
-        <>
-        </>
+        <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='/coffee-list' element={ <CoffeeList /> } />
+            <Route path='/coffee' element={ <Coffee /> } />
+        </Routes>
     );
 }
 
