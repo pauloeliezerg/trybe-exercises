cca6267fcd67201d94827433809caf2b61027081
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-07-18 01:49:26 -0300

    added form tags

--- 02-front-end/02-react-intro/05-forms/01-form/src/App.tsx
+++ 02-front-end/02-react-intro/05-forms/01-form/src/App.tsx
@@ -1,6 +1,29 @@
 function App() {
     return (
         <>
+            <form>
+                <label>
+                    Name
+                    <input />
+                </label>
+                <label>
+                    E-mail
+                    <input />
+                </label>
+                <label>
+                    Schooling
+                    <select>
+                        <option value="high-school">High School</option>
+                        <option value="university">University</option>
+                        <option value="postgraduate">Postgraduate</option>
+                    </select>
+                </label>
+                <label>
+                    Resume
+                    <textarea />
+                </label>
+                <button>Send</button>
+            </form>
         </>
     );
 }
