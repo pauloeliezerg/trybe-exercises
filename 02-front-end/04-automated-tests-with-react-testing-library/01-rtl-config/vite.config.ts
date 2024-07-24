b6cf9d22b6d9529ade74b3bb6f78066460a451d8
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-07-24 03:21:05 -0300

    added Vitest configuration

--- 02-front-end/04-automated-tests-with-react-testing-library/01-rtl-config/vite.config.ts
+++ 02-front-end/04-automated-tests-with-react-testing-library/01-rtl-config/vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './setupTests.ts',
+        css: true,
+        reporters: ['verbose'],
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+        },
+    },
 });
