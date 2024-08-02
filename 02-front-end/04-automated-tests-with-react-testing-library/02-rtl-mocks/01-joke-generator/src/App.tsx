2c429d5069465448d34b6d85348a4e6e3669220c
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-02 05:12:11 -0300

    added code for App Component

--- 02-front-end/04-automated-tests-with-react-testing-library/02-rtl-mocks/01-joke-generator/src/App.tsx
+++ 02-front-end/04-automated-tests-with-react-testing-library/02-rtl-mocks/01-joke-generator/src/App.tsx
@@ -1,9 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+    const [joke, setJoke] = useState('');
+
+    useEffect(() => {
+        async function fetchJoke() {
+            const API_URL = 'https://icanhazdadjoke.com/';
+            const REQUEST_CONFIG = { headers: { Accept: 'application/json' } };
+            const response = await fetch(API_URL, REQUEST_CONFIG);
+            const data = await response.json();
+            setJoke(data.joke);
+        }
+        fetchJoke();
+    }, []);
+
     return (
-        <>
-        </>
+        <div className="App">
+            <p>{joke}</p>
+        </div>
     );
 }
 
