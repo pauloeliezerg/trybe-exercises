61b7cd3c749058c860730d07472a35188d1e0f94
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-02 05:45:36 -0300

    added new joke button click and fetch test

--- 02-front-end/04-automated-tests-with-react-testing-library/02-rtl-mocks/01-joke-generator/src/test/App.test.tsx
+++ 02-front-end/04-automated-tests-with-react-testing-library/02-rtl-mocks/01-joke-generator/src/test/App.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { vi } from 'vitest';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
-describe('Testing App.js', () => {
+describe('Testing App.js fetch', () => {
     it('checks if when rendering the page, a joke is rendered', async () => {
         afterEach(() => vi.clearAllMocks());
         
@@ -25,5 +26,24 @@ describe('Testing App.js', () => {
         expect(renderedJoke).toBeInTheDocument();
         expect(mockFetch).toHaveBeenCalledTimes(1);
         expect(mockFetch).toHaveBeenCalledWith('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
+
+        const MOCK_JOKE_2 = {
+            id: '8s5bGtrOfxc',
+            joke: 'Whats red and bad for your teeth? A Brick.',
+        };
+        
+        const MOCK_RESPONSE_2 = {
+            ok: true,
+            status: 200,
+            json: async () => MOCK_JOKE_2,
+        } as Response;
+
+        const secondMockFetch = vi.spyOn(global, 'fetch').mockResolvedValueOnce(MOCK_RESPONSE_2); 
+        const button = screen.getByRole('button', { name: 'New Joke'});
+        await userEvent.click(button);
+
+        const secondRenderedJoke = await screen.findByText('Whats red and bad for your teeth? A Brick.');
+        expect(secondRenderedJoke).toBeInTheDocument();
+        expect(secondMockFetch).toHaveBeenCalledTimes(1);
     });
 });
