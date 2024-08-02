8ba470428940e701aef5316ec58c4a921acc16f6
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-02 16:55:39 -0300

    added test for rollDice function

--- 02-front-end/04-automated-tests-with-react-testing-library/02-rtl-mocks/02-rpg-battle-ts/src/tests/exercise-01.test.ts
+++ 02-front-end/04-automated-tests-with-react-testing-library/02-rtl-mocks/02-rpg-battle-ts/src/tests/exercise-01.test.ts
@@ -1,7 +1,8 @@
-// import rollDice from '../service/rollDice';
+import rollDice from '../service/rollDice';
+import { vi } from 'vitest';
 
 it('testa a função `rollDice`', () => {
-  /*
-    Exercício 01
-  */
+  vi.spyOn(global.Math, 'random').mockReturnValue(.8);
+  expect(rollDice(20)).toBe(16);
+  expect(global.Math.random).toHaveBeenCalledTimes(1);
 });
