8ca9c3d7ecc428fb52820dc45a23d70387994adb
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-03 01:05:57 -0300

    created tests for attackEnemy function

--- 02-front-end/04-automated-tests-with-react-testing-library/02-rtl-mocks/02-rpg-battle-ts/src/tests/exercise-03.test.ts
+++ 02-front-end/04-automated-tests-with-react-testing-library/02-rtl-mocks/02-rpg-battle-ts/src/tests/exercise-03.test.ts
@@ -1,20 +1,36 @@
-// import rollMultipleDice from '../service/rollMultipleDice';
-// import attackEnemy from '../service/attackEnemy';
+import rollMultipleDice from '../service/rollMultipleDice';
+import attackEnemy from '../service/attackEnemy';
+import { vi } from 'vitest';
+
+vi.mock('../service/rollMultipleDice');
+
+const enemy = {
+  name: 'Chapolin Colorado',
+  defensePoints: 30,
+};
+
+beforeEach(() => vi.resetAllMocks());
 
 it('testa uma vitória na função `attackEnemy`', () => {
-  /*
-    Exercício 03
-  */
+  (rollMultipleDice as any).mockReturnValueOnce(40);
+  const resultObject = attackEnemy(2, 20, enemy);
+
+  expect(resultObject.success).toBe(true);
+  expect(rollMultipleDice).toHaveBeenCalledTimes(1);
 });
 
 it('testa uma derrota na função `attackEnemy`', () => {
-  /*
-    Exercício 03
-  */
+  (rollMultipleDice as any).mockReturnValueOnce(20);
+  const resultObject = attackEnemy(2,20, enemy);
+
+  expect(resultObject.success).toBe(false);
+  expect(rollMultipleDice).toHaveBeenCalledTimes(1);
 });
 
 it('testa um empate nos dados, gerando derrota no `attackEnemy`', () => {
-  /*
-    Exercício 03
-  */
+  (rollMultipleDice as any).mockReturnValueOnce(30);
+  const resultObject = attackEnemy(2,20, enemy);
+
+  expect(resultObject.success).toBe(false);
+  expect(rollMultipleDice).toHaveBeenCalledTimes(1);
 });
