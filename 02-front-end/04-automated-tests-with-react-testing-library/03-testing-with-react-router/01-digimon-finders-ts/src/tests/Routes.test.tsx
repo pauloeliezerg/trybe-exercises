6eceeb5d4e80d969eb613d27315f821afe547e2c
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-06 04:28:36 -0300

    added about heading  test

--- 02-front-end/04-automated-tests-with-react-testing-library/03-testing-with-react-router/01-digimon-finders-ts/src/tests/Routes.test.tsx
+++ 02-front-end/04-automated-tests-with-react-testing-library/03-testing-with-react-router/01-digimon-finders-ts/src/tests/Routes.test.tsx
@@ -2,15 +2,24 @@ import App from '../App';
 import { renderWithRouter } from '../utils/renderWithRouter';
 import { screen } from '@testing-library/react';
 
-describe('Testes das rotas da aplicação', () => {
+describe('Testes das rotas da aplicação', async () => {
   it('A página deverá apresentar o título "Search Digimon"', () => {
     renderWithRouter(<App />);
-
-    const title = screen.getByRole('heading', { name: /Search Digimon/i });
-    const about = screen.getByRole('link', { name: /About/i });
-    const search = screen.getByRole('link', { name: /Search Digimon/i });
-    expect(title).toBeInTheDocument();
-    expect(about).toBeInTheDocument();
-    expect(search).toBeInTheDocument();
+    const homeTitle = screen.getByRole('heading', { name: /Search Digimon/i });
+    expect(homeTitle).toBeInTheDocument();
+  });
+  it('A página deverá apresentar os links "About" e "Search Digimon"', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    const searchLink = screen.getByRole('link', { name: /Search Digimon/i });
+    expect(aboutLink).toBeInTheDocument();
+    expect(searchLink).toBeInTheDocument();
+  });
+  it('Ao clicar no link About, a pessoa usuária é direcionada para a página About. Teste se o título "About" será renderizado na tela', async () => {
+    const { user } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    await user.click(aboutLink);
+    const aboutTitle = screen.getByRole('heading', { name: /About/i });
+    expect(aboutTitle).toBeInTheDocument();
   });
 });
