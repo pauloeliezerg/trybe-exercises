64ba10407f8e21bd6728d98437a86699634fb40a
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-07 02:35:33 -0300

    added fetch function test

--- 02-front-end/04-automated-tests-with-react-testing-library/03-testing-with-react-router/01-digimon-finders-ts/src/tests/Search.test.tsx
+++ 02-front-end/04-automated-tests-with-react-testing-library/03-testing-with-react-router/01-digimon-finders-ts/src/tests/Search.test.tsx
@@ -14,8 +14,35 @@ describe('Testes de busca por Digimon', async () => {
     const renderedDigimon = screen.queryByTestId('digimonName');
     expect(renderedDigimon).not.toBeInTheDocument();
   });
-  it('É possível buscar um Digimon com sucesso', () => {
+  it('É possível buscar um Digimon com sucesso', async () => {
+    const DIGIMON_MOCK = [
+      {
+        name: 'Agumon',
+        level: 'Rookie',
+        img: 'https://digimon.shadowsmith.com/img/agumon.jpg',
+      }
+    ];
 
+    global.fetch = vi.fn(() => Promise.resolve({
+      json: () => Promise.resolve(DIGIMON_MOCK),
+    })) as any;
+
+    const { user } = renderWithRouter(<App />);
+    expect(global.fetch).toHaveBeenCalledTimes(0);
+    const input = screen.getByRole('textbox', { name: /Digimon/i });
+    const button = screen.getByRole('button', { name: /Search Digimon/i });
+    await user.type(input, 'Agumon');
+    await user.click(button);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toBeCalledWith(
+      'https://digimon-api.vercel.app/api/digimon/name/Agumon',
+    );
+    const digimonLevel = await screen.findByText('Level: Rookie');
+    expect(digimonLevel).toBeInTheDocument();
+    const digimonName = await screen.findByText(/Agumon/i);
+    expect(digimonName).toBeInTheDocument();
+    const digimonImage = await screen.findByText('Agumon');
+    expect(digimonImage).toBeInTheDocument();
   });
   it('A mensagem de erro é renderizada caso o Digimon buscado não exista', () => {
 
