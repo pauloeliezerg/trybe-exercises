a00e2f5415370fc80c1a50970466895f0c89e2ad
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-10 22:31:21 -0300

    added eventListeners

--- 02-front-end/06-redux/01-redux-intro/01-exercise-redux-intro/src/main.ts
+++ 02-front-end/06-redux/01-redux-intro/01-exercise-redux-intro/src/main.ts
@@ -1,13 +1,16 @@
+import store from "./redux/store";
+import { nextColor, previousColor } from './redux/actions';
+
 const btnPrevious = document.getElementById('previous') as HTMLButtonElement;
 const btnNext = document.getElementById('next') as HTMLButtonElement;
 const btnRandom = document.getElementById('random') as HTMLButtonElement;
 
 btnPrevious.addEventListener('click', () => {
-  // realiza o dispatch da action
+  store.dispatch(previousColor());
 });
 
 btnNext.addEventListener('click', () => {
-  // realiza o dispatch da action
+  store.dispatch(nextColor());
 });
 
 // realiza o subscribe da store
