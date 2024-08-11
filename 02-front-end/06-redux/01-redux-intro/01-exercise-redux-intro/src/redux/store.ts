83cd730fbea277631897613e7cb97b3eb2814b70
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-10 22:21:52 -0300

    created reducer with switch

--- 02-front-end/06-redux/01-redux-intro/01-exercise-redux-intro/src/redux/store.ts
+++ 02-front-end/06-redux/01-redux-intro/01-exercise-redux-intro/src/redux/store.ts
@@ -1,13 +1,7 @@
 import { legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension/lib/types/logOnly';
-import { INITIAL_STATE } from './reducer';
-
-type ActionType = {
-  type: string,
-};
-
-const reducer = (state = INITIAL_STATE, action: ActionType) => {
-  return state;
-}
+import reducer from './reducer';
 
 const store = createStore(reducer, composeWithDevTools());
+
+export default store;
