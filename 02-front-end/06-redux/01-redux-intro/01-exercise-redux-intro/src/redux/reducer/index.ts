ac5174e1390828a0a8447443d67796071a4d8f7e
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-10 22:26:49 -0300

    added switch conditions to change global state

--- 02-front-end/06-redux/01-redux-intro/01-exercise-redux-intro/src/redux/reducer/index.ts
+++ 02-front-end/06-redux/01-redux-intro/01-exercise-redux-intro/src/redux/reducer/index.ts
@@ -7,6 +7,16 @@ const INITIAL_STATE = {
 
 const reducer = (state = INITIAL_STATE, action: AnyAction ) => {
   switch (action.type) {
+    case 'NEXT_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? 0 : state.index + 1,
+      }
+    case 'PREVIOUS_COLOR':
+      return {
+        ...state,
+        index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
+      }
     default:
       return state;
   }
