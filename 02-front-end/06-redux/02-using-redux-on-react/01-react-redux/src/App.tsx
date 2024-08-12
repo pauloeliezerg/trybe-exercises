2d91332a1a05d6b4286e3f5a38acf6cef22584be
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-11 22:35:23 -0300

    added useSelector hook

--- 02-front-end/06-redux/02-using-redux-on-react/01-react-redux/src/App.tsx
+++ 02-front-end/06-redux/02-using-redux-on-react/01-react-redux/src/App.tsx
@@ -1,8 +1,16 @@
+import { useSelector } from 'react-redux';
+
+type RootStateType = {
+  count: number;
+};
+
 function App() {
+  const rootState = useSelector((state: RootStateType) => state);
+
   return (
     <>
       <h1>Contador</h1>
-      <h2>0</h2>
+      <h2>{ rootState.count }</h2>
       <button>Incrementa 1</button>
       <button>Incrementa 5</button>
     </>
