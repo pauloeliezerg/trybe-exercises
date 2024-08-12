0039bbb478c949d00d26d7f7c33f1247b3de9d8d
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-11 22:57:57 -0300

    added useDispatch hook

--- 02-front-end/06-redux/02-using-redux-on-react/01-react-redux/src/App.tsx
+++ 02-front-end/06-redux/02-using-redux-on-react/01-react-redux/src/App.tsx
@@ -1,4 +1,6 @@
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { actionCreator } from './redux/actions';
 
 type RootStateType = {
   count: number;
@@ -6,13 +8,14 @@ type RootStateType = {
 
 function App() {
   const rootState = useSelector((state: RootStateType) => state);
+  const dispatch = useDispatch();
 
   return (
     <>
       <h1>Contador</h1>
       <h2>{ rootState.count }</h2>
-      <button>Incrementa 1</button>
-      <button>Incrementa 5</button>
+      <button onClick={ () => dispatch(actionCreator()) }>Incrementa 1</button>
+      <button onClick={ () => dispatch(actionCreator(5)) }>Incrementa 5</button>
     </>
   )
 }
