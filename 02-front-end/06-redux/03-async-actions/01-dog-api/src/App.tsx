4b78e3a3a3bb1b46bec8671d71095e9ca51b36c3
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-13 15:03:36 -0300

    implemented App

--- 02-front-end/06-redux/03-async-actions/01-dog-api/src/App.tsx
+++ 02-front-end/06-redux/03-async-actions/01-dog-api/src/App.tsx
@@ -1,8 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch, ReduxState } from "./types";
+import { fetchDogImage } from "./redux/actions";
+
 function App() {
+  const rootState = useSelector((state: ReduxState) => state);
+  const dispatch: Dispatch = useDispatch();
+
+  if (rootState.isFetching) return <p>Carregando...</p>
+
   return (
     <>
+      <button
+        onClick={ () => { dispatch(fetchDogImage()) } }
+      >
+        Novo Doguinho
+      </button>
+      {
+        rootState.imageURL
+        && <img
+          src={ rootState.imageURL }
+          alt="Imagem de um cachorro aleatório"
+        />
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
