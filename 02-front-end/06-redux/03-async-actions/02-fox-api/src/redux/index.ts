5a35642c74eaacd10595a790d99c88fa320f7bc0
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-15 22:13:35 -0300

    added thunk to store

--- 02-front-end/06-redux/03-async-actions/02-fox-api/src/redux/index.ts
+++ 02-front-end/06-redux/03-async-actions/02-fox-api/src/redux/index.ts
@@ -1,7 +1,7 @@
-import { legacy_createStore as createStore } from 'redux';
-
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import gallery from './reducers/gallery';
+import { thunk } from 'redux-thunk';
 
-const store = createStore(gallery);
+const store = createStore(gallery, applyMiddleware(thunk));
 
 export default store;
