8b1283a0fbf13784e5ae3287675ad84398fa10a7
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-22 15:00:14 -0300

    added more button tests (#57)

--- 02-front-end/06-redux/04-react-redux-tests/01-contador/src/tests/App.test.tsx
+++ 02-front-end/06-redux/04-react-redux-tests/01-contador/src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react';
 import renderWithRedux from '../utils/renderWithRedux';
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
 
 import App from '../App';
 
@@ -21,22 +21,26 @@ describe('Testa a aplicação contador', () => {
   });
 
   it('Incrementa corretamente ao clicar no botão', async () => {
-    const { store, user } = renderWithRedux(<App />);
-
-    const buttonIncrement1 = screen.getByRole('button', { name: 'Incrementa 1' });
-    const buttonIncrement5 = screen.getByRole('button', { name: 'Incrementa 5' });
-    await user.click(buttonIncrement1);
-    await user.click(buttonIncrement5);
-
-    interface RootState {
+    type RootState = {
       counterReducer?: {
         count: number;
       };
     };
 
-    const state = store.getState() as RootState;
-    const count = state.counterReducer?.count ?? 0;
+    const { store, user } = renderWithRedux(<App />);
 
+    const buttonIncrementOne = screen.getByRole('button', { name: 'Incrementa 1' });
+    const buttonIncrementFive = screen.getByRole('button', { name: 'Incrementa 5' });
+
+    await user.click(buttonIncrementOne);
+    let state = store.getState() as RootState;
+    let count = state.counterReducer?.count ?? 0;
+    expect(screen.getByText('1')).toBeInTheDocument();
+    expect(count).toBe(1);
+
+    await user.click(buttonIncrementFive);
+    state = store.getState() as RootState;
+    count = state.counterReducer?.count ?? 0;
     expect(screen.getByText('6')).toBeInTheDocument();
     expect(count).toBe(6);
   });
@@ -52,4 +56,27 @@ describe('Testa a aplicação contador', () => {
     expect(screen.queryByText('0')).not.toBeInTheDocument();
     expect(screen.getByText('10')).toBeInTheDocument();
   });
-})
+
+  it('Incrementa corretamente ao clicar no botão com estado global diferente de 0', async () => {
+    const state = {
+      counterReducer: {
+        count: 5,
+      },
+    };
+
+    const {  user } = renderWithRedux(<App />, state);
+    const buttonIncrementOne = screen.getByRole('button', { name: 'Incrementa 1'});
+    const buttonIncrementFive = screen.getByRole('button', { name: 'Incrementa 5'});
+  
+    expect(screen.queryByText('0')).not.toBeInTheDocument();
+    expect(screen.getByText('5')).toBeInTheDocument();
+
+    await user.click(buttonIncrementOne);
+    expect(screen.queryByText('0')).not.toBeInTheDocument();
+    expect(screen.getByText('6')).toBeInTheDocument();
+    
+    await user.click(buttonIncrementFive);
+    expect(screen.queryByText('0')).not.toBeInTheDocument();
+    expect(screen.getByText('11')).toBeInTheDocument();
+  });
+});
