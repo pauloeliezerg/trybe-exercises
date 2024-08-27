6e37043d72243c24c4fc214f1620f2872b155d4e
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-27 17:13:56 -0300

    added profile page tests (direct url with and w/o global state changes)

--- 02-front-end/06-redux/04-react-redux-tests/02-greetings-page/src/tests/App.test.tsx
+++ 02-front-end/06-redux/04-react-redux-tests/02-greetings-page/src/tests/App.test.tsx
@@ -16,3 +16,24 @@ test('Ao acessar a rota /profile a partir da Home, o nome da pessoa deverá ser 
   await user.click(loginButton);
   expect(screen.getByText('Boas vindas, Tryber')).toBeInTheDocument();
 });
+
+test('Ao acessar diretamente a rota /profile, o nome da pessoa não pode ser renderizado', () => {
+  const route = '/profile';
+
+  renderWithRouterAndRedux(<App />, route);
+
+  expect(screen.queryByText('Boas vindas')).not.toBeInTheDocument();
+  expect(screen.getByText('Você precisa realizar o login')).toBeInTheDocument();
+});
+
+test('Ao acessar diretamente a rota /profile e alterando o estado global, o nome da pessoa deve ser renderizado', () => {
+  const route = '/profile';
+  const state = { userName: 'Tryber' };
+
+  renderWithRouterAndRedux(<App />, route, state);
+
+  expect(
+    screen.queryByText('Você precisa realizar o login')
+  ).not.toBeInTheDocument();
+  expect(screen.getByText('Boas vindas, Tryber')).toBeInTheDocument();
+});
