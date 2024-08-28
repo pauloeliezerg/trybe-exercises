9609ffaff8e69eef7b7849ef45d4c27944c03982
Paulo Eliezer <pauloeliezerg@gmail.com>
2024-08-28 16:28:53 -0300

    added exercise 3 (test for mocked function and button click return)

--- 02-front-end/06-redux/04-react-redux-tests/03-exercise-pokemon-card-ts/src/App.test.tsx
+++ 02-front-end/06-redux/04-react-redux-tests/03-exercise-pokemon-card-ts/src/App.test.tsx
@@ -1,6 +1,6 @@
 import { screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import { vi } from 'vitest';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import mockFetch from './__mocks__/mockFetch';
 import renderWithRedux from './helpers/renderWithRedux';
 import App from './App';
@@ -33,18 +33,39 @@ describe('Página principal', () => {
   test('2 - Verifica se foi feita uma requisição à API após carregar a página', async () => {
     const firstPokemonId = 656;
     const firstEndPoint = 'https://pokeapi.co/api/v2/pokemon/656/';
-
+    
     (randomNumber as any).mockReturnValue(firstPokemonId);
+    
+    renderWithRedux(<App />);
+    await waitForElementToBeRemoved(() => screen.getByText(LOADING_TEXT));
+    
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(firstEndPoint);
+  });
+  
+  test('3 - Verifica se o endpoint da requisição é alterado ao clicar no botão', async () => {
+    const firstPokemonId = 656;
+    const firstEndPoint = 'https://pokeapi.co/api/v2/pokemon/656/';
+    
+    const secondPokemonId = 96;
+    const secondEndPoint = 'https://pokeapi.co/api/v2/pokemon/96/';
+
+    (randomNumber as any).mockReturnValue(secondPokemonId);
+    (randomNumber as any).mockReturnValueOnce(firstPokemonId);
 
     renderWithRedux(<App />);
     await waitForElementToBeRemoved(() => screen.getByText(LOADING_TEXT));
 
     expect(global.fetch).toHaveBeenCalledTimes(1);
     expect(global.fetch).toHaveBeenCalledWith(firstEndPoint);
-  });
+    
+    const button = await screen.findByRole('button');
 
-  test.skip('3 - Verifica se o endpoint da requisição é alterado ao clicar no botão', async () => {
-    // Remova o 'skip' e implemente seu teste
+    userEvent.click(button);
+    await waitForElementToBeRemoved(() => screen.getByText(LOADING_TEXT));
+    
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+    expect(global.fetch).toHaveBeenCalledWith(secondEndPoint);
   });
 
   test.skip('4 - Verifica se os elementos contendo as informações do Pokémon são renderizados', async () => {
